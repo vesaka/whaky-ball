daf8337325c62b9cb5cf83a049b0882b48e8c34c
Veselin <vesakabgr@gmail.com>
2023-06-23 08:30:46 +0300

    added default ciruit props

--- models/circuits/circuit.js
+++ models/circuits/circuit.js
@@ -1,22 +1,53 @@
 import Model from '$core/3d/models/model.js';
-
+import CircuitBuilder from '$wb/system/circuit-builder.js';
 import { CatmullRomCurve3, Vector3 } from 'three';
+import { Body, Shape, Vec3, Box, Trimesh } from 'cannon-es';
 
 class Circuit extends Model {
 
-    createRandomCircuit(width = 480, height = 360, numPoints = 20, amplitude = 10) {
-        const points = [];
-        const segmentWidth = width / numPoints;
-        const centerX = width / 2;
-        const centerY = height / 2;
+    constructor(options = {}) {
+        
+        super(options);
+    }
 
-        for (let i = 0; i < numPoints; i++) {
-            const x = i * segmentWidth;
-            const y = centerY + (Math.random() * amplitude * 2 - amplitude);
-            points.push({ x, y });
+    createRandomCircuit() {
+        const points = this.points.map(point => new Vector3(point.x, point.y, 0));
+        return new CatmullRomCurve3(points);
+    }
+
+    createPoints() {
+        const {width, height, depth} = this;
+        const builder = new CircuitBuilder();
+        return builder.createPoints();
+    }
+
+    getPoints() {
+        if (!this.points) {
+            const builder = new CircuitBuilder();
+            this.points = builder.createPoints();
         }
 
-        return new CatmullRomCurve3(points);
+        return this.points;
+    }
+
+    createBody() {
+        return new Body({
+            mass: 0,
+            type: Body.STATIC,
+            allowSleep: true,
+            shape: new Box(new Vec3(1, 1, 1)),
+            position: new Vec3(0, 0, 0)
+        });
+        return new Body({
+            mass: 0,
+            position: new Vec3(0, 0, 0),
+            shape: new Shape({
+                type: Shape.types.TRIMESH,
+                vertices: this.points,
+                indices: this.builder.createIndices(this.points.length),
+                edges: this.builder.createEdges(this.points.length)
+            })
+        });
     }
 }
 
