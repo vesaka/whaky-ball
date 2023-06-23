f5f0b153971db213dd16286d72941946cd93a53b
Veselin <vesakabgr@gmail.com>
2023-06-23 08:27:44 +0300

    dynamic load

--- system/level.js
+++ system/level.js
@@ -1,8 +1,9 @@
 import Container from '$core/container';
 
-import PlaneCircuit from '$wb/models/plane-circuit.js';
-import TrackCircuit from '$wb/models/track-circuit.js';
-import RollerCoasterCircuit from '$wb/models/roller-coaster-circuit.js';
+import PlaneCircuit from '$wb/models/circuits/plane-circuit.js';
+import TrackCircuit from '$wb/models/circuits/track-circuit.js';
+import RollerCoasterCircuit from '$wb/models/circuits/roller-coaster-circuit.js';
+import { raw, extend } from '$core/utils/object';
 
 const circuits = {
     plane: PlaneCircuit,
@@ -12,13 +13,22 @@ const circuits = {
 
 class Level extends Container {
 
-    load(level = 1) {
+    constructor(options = {}) {
+        super(options);
+        this.gui.addFolder('Level', this.options.models);
+    }
 
+    load(level = 1, options = {}) {
+        this.circuit = this.getCircuit(options);
+        this.$emit('level_ready', this.circuit);
+        
     }
 
     getCircuit(name, options = {}) {
+        const { def, types } = this.options.models.circuits;
         const circuitClass = circuits[name] || Object.values(circuits)[0];
-        return new circuitClass(options);
+        const settings = extend(def, types[name] || {});
+        return new circuitClass(extend(settings, options));
     }
 }
 
